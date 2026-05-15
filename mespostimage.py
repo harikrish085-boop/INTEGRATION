404a6ac71f61b3d0b5149f9935905e7972b3cb6b
HariKrishna <hari.krish085@gmail.com>
2026-05-15 16:12:31 +0800

    post image into db and get image from db via rest api

---
+++ mespostimage.py
@@ -0,0 +1,34 @@
+import requests
+import base64
+import os
+from datetime import datetime
+imagename = "batteries-09-00111-g007-550.jpg"
+image_dir =os.path.join(os.getcwd(),"images")
+image_path = os.path.join(image_dir,imagename)
+
+
+with open(image_path,"rb") as f:
+    img = base64.b64encode(f.read()).decode()
+
+payload = {
+    "machineId":"M007",
+    "imageName":imagename,
+    "imageType":"jpg",
+    "createdAt":datetime.now().isoformat(),
+    "imageData":img
+}
+####### Post image to MESIntegration API locally running on port 8088 ########
+#r = requests.post(
+#    "http://localhost:8088/system/webdev/samplequickstart/MESIntegration/api/images/postImage",
+#    json=payload
+#)
+###### Post image to MESIntegration API DEV server running on port 8088 ########
+r = requests.post(
+    "http://10.34.26.4:8088/system/webdev/samplequickstart/MES_INTEGRATION_REST_API/API/images/postImage",
+    json=payload
+)
+
+#print(r.json())
+print(r.status_code)
+print(r)
+print("Image posted successfully")  
